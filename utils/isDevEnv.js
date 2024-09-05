bb3a72307ebc0ea7327066576723d5090015991b
Dekita <dekitarpg@gmail.com>
2024-09-05 13:26:16 +0100

    Update isDevEnv.js

--- utils/isDevEnv.js
+++ utils/isDevEnv.js
@@ -2,11 +2,8 @@
 ########################################
 # PalHUB::Client by dekitarpg@gmail.com
 ########################################
+returns true / false if the environment is development
 */
-let isDevEnvironment = false;
-
-if (process && process.env.NODE_ENV === 'development') {
-    isDevEnvironment = true;
+export default function isDevEnvironment() {
+    return process && process.env.NODE_ENV === 'development';
 }
-
-export { isDevEnvironment };
